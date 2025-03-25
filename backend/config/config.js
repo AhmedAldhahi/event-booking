30fa41751ff819723c7b5f9199ac55837053432f
Ahmed Aldhahi <ahmed.aldhahi.a@gmail.com>
2025-03-25 20:42:21 +0300

    Add production config for Render

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,19 +1,22 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT || 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+const commonConfig = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT || 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
   }
+};
+
+module.exports = {
+  development: commonConfig,
+  production: commonConfig
 };
